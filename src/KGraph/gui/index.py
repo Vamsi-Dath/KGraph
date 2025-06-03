d2fc82c4cad16c20366ef5a5ea66629554b1c880
Vamsi Dath <vamsi@Vamsis-MacBook-Air.local>
2025-06-02 22:57:16 -0500

    Added LLM Config. and updated file indexing fn.

--- src/KGraph/gui/index.py
+++ src/KGraph/gui/index.py
@@ -1,26 +1,91 @@
 import gradio as gr
+import os
+from pathlib import Path
+import datetime
+import time
 
-def load_file(file):
-    if not file:
+_CURRENT_DATABASE = None
+
+def load_file(files):
+    if not files:
         return
-    with open(file, "r") as f:
-        contents = f.read()
-    return contents
+    contents = ""
+    for file in files:
+        with open(file, "r") as f:
+            contents += "-"*50 + "\n👉 " + file + "\n\n"
+            yield contents
+            time.sleep(0.5)
+            contents += f.read()
+            yield contents
+            contents += "\n\n"
+            time.sleep(1)
+    return
+
+def index_file_uploaded(
+    file_path:list[str],
+    base_url: str | None = None,
+    api_key: str | None = None,
+    llm_model: str | None = None,
+):
+    if not file_path:
+        yield "Upload file to continue."
+        return
+
+    if base_url:
+        os.environ["BASE_URL"] = base_url
+    if api_key:
+        os.environ["API_KEY"] = api_key
+    if llm_model:
+        os.environ["LLM_MODEL"] = llm_model
+    
+
+    result_log = (
+        "Program configs:\n"
+        + f"BASE_URL: {base_url}\n\nAPI_KEY: ....\n\nLLM_MODEL: {llm_model}\n\nDatabase: {_CURRENT_DATABASE}\n"
+    )
+    yield result_log
+    result_log += f"\nStart indexing {[Path(p).name for p in file_path].__str__()} at {datetime.datetime.now()}...\n"
+    yield result_log
+
+    # Implement the indexing logic
+    
+    result_log += "\nSave to database...\n"
+    result_log += f"END!"
+    yield result_log
+    return
+
 
 with gr.Blocks() as index:
     gr.Markdown('# Process Data')
     with gr.Row():
-        with gr.Column(scale=1):
-            gr.Markdown('## Upload Data')
-            files = gr.File(
-                label="Allowed file types .json or .txt",
-                file_types=[".json", ".txt"],
-                file_count='single',
-                render=True,
-                type="filepath"
-            )
+        with gr.Row():
+            with gr.Column(scale=1):
+                gr.Markdown('## Upload Data')
+                files = gr.File(
+                    label="Allowed file types .json",
+                    file_types=[".json"],
+                    file_count="multiple",
+                    render=True,
+                    type="filepath"
+                )
 
-        with gr.Column(scale=2):
+            with gr.Accordion(label="LLM Configuration", open=False):
+                    base_url = gr.Textbox(
+                        placeholder="baseurl", label="LLM Baseurl"
+                    )
+                    api_key = gr.Textbox(
+                        placeholder="API key",
+                        label="API Key",
+                        type="password",
+                    )
+                    llm_model = gr.Dropdown(
+                        ["gpt-4o-mini", "gpt-4o", "claude-3.5-sonnet"],
+                        allow_custom_value=True,
+                        label="LLM Model",
+                        interactive=True,
+                    )
+
+        with gr.Column(scale=1):
             gr.Markdown('## Data Preview')
             data_preview = gr.Textbox(
                 label="Preview",
@@ -31,16 +96,21 @@ with gr.Blocks() as index:
 
         files.change(fn=load_file, inputs=files, outputs=data_preview)
 
-        with gr.Column(scale=1):
+        with gr.Column(scale=2):
             button = gr.Button(
-                "Process Data",
+                "Start Indexing",
                 variant="primary",
                 interactive=True
             )
             alert_msg = gr.Textbox(label="Status", interactive=False)
 
     button.click(
-        lambda files: "Success!" if files else "No files uploaded.",
-        inputs=files,
+        index_file_uploaded,
+        inputs=[
+            files,
+            base_url,
+            api_key,
+            llm_model
+        ],
         outputs=alert_msg
     )
